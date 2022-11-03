b12f0bcfe1a4acdae4e60b627df1bed683eeb867
VTrofym <v.trof@icloud.com>
2022-11-03 20:19:05 +0200

    added filterContact on redux

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,4 @@
 import { ContactList } from 'components/ContactList';
-import { useState } from 'react';
 import { nanoid } from 'nanoid';
 
 import { ContactForm } from 'components/ContactForm';
@@ -7,24 +6,21 @@ import { Filter } from 'components/Filter';
 import css from './App.module.css';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { addContactAction, deleteContactAction } from 'redux/contacts/action';
+import {
+  addContactAction,
+  deleteContactAction,
+  filterContactAction,
+} from 'redux/contacts/action';
 const ID_LOCAL_KEY = 'active-id';
 
 export const App = () => {
   const dispatch = useDispatch();
   const contacts = useSelector(state => state.contacts);
-  // const [contacts, setContacts] = useState([
-  //   { id: '1', name: 'John Bold', number: '(067)-323-43-43' },
-  //   { id: '2', name: 'Jane Gold', number: '(066)-123-24-43' },
-  //   { id: '3', name: 'Jim Fold', number: '(097)-456-40-39' },
-  //   { id: '4', name: 'Jack Daniels', number: '(050)-488-48-38' },
-  // ]);
-  const [filter, setFilter] = useState('');
+  const filter = useSelector(state => state.filter);
 
   useEffect(() => {
     const savedContacts = JSON.parse(localStorage.getItem(ID_LOCAL_KEY));
     if (savedContacts) {
-      // setContacts(savedContacts);
     }
   }, []);
 
@@ -41,28 +37,14 @@ export const App = () => {
       return alert(`${name} is already in contacts!`);
     }
     dispatch(addContactAction({ name, number, id: nanoid() }));
-
-    // setContacts(prevContacts => {
-    //   return [
-    //     ...prevContacts,
-    //     {
-    //       name: name,
-    //       number: number,
-    //       id: nanoid(),
-    //     },
-    //   ];
-    // });
   };
 
   const deleteContact = event => {
     dispatch(deleteContactAction(event.target.id));
-    // setContacts(prevContact => {
-    //   return prevContact.filter(contact => contact.id !== event.target.id);
-    // });
   };
 
   const filterContact = event => {
-    setFilter(event.currentTarget.value);
+    dispatch(filterContactAction(event.currentTarget.value));
   };
 
   const getVisibleContacts = () => {
