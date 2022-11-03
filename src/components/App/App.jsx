dee517abb48d4e0154517ab3aa525813fee44b54
VTrofym <v.trof@icloud.com>
2022-11-03 21:26:45 +0200

    fixed bugs

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,11 +4,7 @@ import { nanoid } from 'nanoid';
 import { ContactList } from 'components/ContactList';
 import { ContactForm } from 'components/ContactForm';
 import { Filter } from 'components/Filter';
-import {
-  addContactAction,
-  deleteContactAction,
-  filterContactAction,
-} from 'redux/contacts/action';
+import { addContactAction, deleteContactAction } from 'redux/contacts/action';
 import css from './App.module.css';
 
 const ID_LOCAL_KEY = 'active-id';
@@ -17,7 +13,6 @@ export const App = () => {
   const dispatch = useDispatch();
 
   const contacts = useSelector(state => state.contacts);
-  const filter = useSelector(state => state.filter);
 
   useEffect(() => {
     const savedContacts = JSON.parse(localStorage.getItem(ID_LOCAL_KEY));
